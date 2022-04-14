9cdee6ff43a7a5640f7701390544c532137add17
Rory Nguyen <dnguyen2048@gmail.com>
2022-04-14 18:27:35 -0500

    part 9 done

--- lab03/answers.sql
+++ lab03/answers.sql
@@ -10,42 +10,52 @@ FROM users;
 
 
 -- Exercise 3
-
+SELECT id, first_name, last_name
+FROM users
+ORDER BY last_name
 
 
 
 -- Exercise 4
-
-
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id=26
 
 
 -- Exercise 5
-
-
+SELECT id, image_url, user_id
+FROM posts
+WHERE user_id=26 OR user_id=12
 
 
 -- Exercise 6
-
-
+SELECT COUNT(*) FROM posts
 
 
 -- Exercise 7
-
-
+SELECT user_id, count(*) FROM comments
+GROUP BY user_id
+ORDER BY count(*) desc
 
 
 -- Exercise 8
-
+SELECT posts.id, posts.image_url, posts.user_id, users.username, users.first_name, users.last_name
+FROM posts
+INNER JOIN users ON posts.user_id = users.id
+WHERE posts.user_id=26 OR posts.user_id=12
 
 
 
 -- Exercise 9
-
+SELECT posts.id, posts.pub_date, following.following_id
+FROM posts
+INNER JOIN following ON posts.user_id=following.following_id
+WHERE following.user_id=26
 
 
 
 -- Exercise 10
-
+--optional :)
 
 
 
